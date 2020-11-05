bedc9d713a04b7b1ef0d61f3ffcb5e605840362f
허성범 <35191431+heosungbum@users.noreply.github.com>
2020-11-05 18:07:13 +0900

    .

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # MiniGame
  
+## 설정
+app.js가 있는 폴더 내에 setting.json 파일이 있어야 작동할 수 있습니다. 
+```
+{
+    "token": "토큰을 여기에 입력하세요."
+}
+```
+
+### 토큰
+토큰입니다. 이것은 디스코드 봇을 사용하기 때문에 봇 토큰이 필수적입니다. "token" 으로 지정해 주어야 봇 토큰으로 인식할 수 있습니다.
+
+## 명령어
+자유롭게 추가하실 수 있습니다. 당연하게도요.
+### 추가 방식
+commands 폴더 내에 넣으셔야 합니다.
+```
+const Discord = require('discord.js');
+
+exports.run = (client, msg, args) => {
+    //명령어 내용
+};
+exports.name = "명령어 이름";
+```
--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const React =require('react');
 const Discord = require('discord');
 const client = Discord.Client();
+const setting = require('./setting.json');
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+client.commands.load = dir => {
+    for (const file of fs.readdirSync(dir)) {
+        const cmd = require(`./commands/${file}`);
+        client.commands.set(cmd.name, cmd);
+      }
+      console.log(client.commands.map(c => c.name).join(', ') + ' 명령어가 로드.');
+}
+
+client.commands.load(__dirname + "/commands");
+
+
+const prefix = setting.prefix;
 
 client.on('ready', ()=>{
     console.log(`ready, Log in as ${client.user.tag}.`);
 });
 
+client.on('message', ()=>{
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return;
+    if (msg.content.slice(0, prefix.length) !== prefix) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    let cmdC = client.commands.get(cmd);
+
+    if(cmdC) cmdC.run(client, msg, args);
+});
+
+client.run(setting.token);
