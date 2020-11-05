c766ca3ec1dbff445185ad219bc0329fae4c2b8f
허성범 <35191431+heosungbum@users.noreply.github.com>
2020-11-05 20:24:44 +0900

    pong bug fix

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, msg, args) => {
-    const embed = new Discord.MessageEmbed().setColor('BLUE').setTitle('PONG!').setDescription('Ping Pong!').addField('', `${client.ws.ping}`);
+    const embed = new Discord.MessageEmbed().setColor('BLUE').setTitle('PONG!').addField('ms', `${client.ws.ping}`, true);
     msg.channel.send(embed);
 };
 exports.name = "ping";
